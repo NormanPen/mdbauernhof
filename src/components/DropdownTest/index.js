4a9a5b6229696bfe74823f8aba9b3b038d2467c6
Norman Pendzich <73986750+NormanPen@users.noreply.github.com>
2021-10-27 08:02:39 +0200

    dsgs

---
+++ src/components/DropdownTest/index.js
@@ -0,0 +1,24 @@
+import "./style.css";
+
+const DropdownTest = () => {
+  return (
+    <div className="bg-black text-white h-full overflow-y-scroll overflow-x-hidden fixed z-50 w-full">
+      <ul>
+        <li>
+          <p className="pt-52"> jsadjo</p>
+        </li>
+        <li>
+          <p className="pt-52"> jsadjo</p>
+        </li>
+        <li>
+          <p className="pt-52"> jsadjo</p>
+        </li>
+        <li>
+          <p className="pt-52"> jsadjo</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default DropdownTest;
