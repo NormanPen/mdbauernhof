c8b2f2627e30c7e7c6d2628066ee35ab9543d658
Norman Pendzich <73986750+NormanPen@users.noreply.github.com>
2022-01-03 16:42:27 +0100

    Refraktoring

--- src/components/HeaderContact/index.js
+++ src/components/HeaderContact/index.js
@@ -1,24 +1,31 @@
-const HeaderContact = (props) => {
+const HeaderContact = ({
+  desktopBgImgSrc,
+  desktopBgImgAlt,
+  imgSrc,
+  imgAlt,
+  heading,
+  children,
+}) => {
   return (
     <div>
-      <div className="">
+      <div className="HeaderContact">
         <div className="relative mx-auto ">
           <img
             className="absolute right-0 hidden -top-24 z-10 md:block md:max-w-3xl lg:max-w-full "
-            src={props.desktopBgImgSrc}
-            alt={props.desktopBgImgAlt}
+            src={desktopBgImgSrc}
+            alt={desktopBgImgAlt}
           />
         </div>
         <header className="relative z-10 text-center ">
           <div className="text-center">
             <div className="flex justify-center pt-20 mb-8">
-              <img src={props.imgSrc} alt={props.imgAlt} />
+              <img src={imgSrc} alt={imgAlt} />
             </div>
 
-            <h1 className="text-4xl font-medium font-Atma">{props.heading}</h1>
+            <h1 className="text-4xl font-medium font-Atma">{heading}</h1>
             <div className="flex justify-center">
               <p className=" pt-5 font-Signika-Negative max-w-xl px-5">
-                {props.children}
+                {children}
               </p>
             </div>
           </div>
