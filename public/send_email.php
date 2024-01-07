cb369e3d4fa5920eeef549b939ba1fba9b365804
Norman Pendzich <73986750+NormanPen@users.noreply.github.com>
2024-01-07 14:47:23 +0100

    Changes from 07.01.2024

--- public/send_email.php
+++ public/send_email.php
@@ -3,21 +3,21 @@
 $email_from = "absender@domain.de";   //Absender falls keiner angegeben wurde
 $sendermail_antwort = true;      //E-Mail Adresse des Besuchers als Absender. false= Nein ; true = Ja
 $name_von_emailfeld = "email";   //Feld in der die Absenderadresse steht
- 
-$empfaenger = "mdbauernhof@aol.com"; //Empfänger-Adresse
+
+$empfaenger = "MDBauernhof@t-online.de"; //Empfänger-Adresse
 $mail_cc = ""; //CC-Adresse, diese E-Mail-Adresse bekommt einer weitere Kopie
 $betreff = "Neue Kontaktanfrage"; //Betreff der Email
- 
+
 $url_ok = "https://www.mdbauernhof.de/#"; //Zielseite, wenn E-Mail erfolgreich versendet wurde
 $url_fehler = "https://www.mdbauernhof.de/#/mailfehler"; //Zielseite, wenn E-Mail nicht gesendet werden konnte
- 
- 
+
+
 //Diese Felder werden nicht in der Mail stehen
 $ignore_fields = array('submit');
- 
- 
- 
- 
+
+
+
+
 //Datum, wann die Mail erstellt wurde
 $name_tag = array("Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag");
 $num_tag = date("w");
@@ -26,10 +26,10 @@ $jahr = date("Y");
 $n = date("d");
 $monat = date("m");
 $time = date("H:i");
- 
+
 //Erste Zeile unserer Email
 $msg = ":: Gesendet am $tag, den $n.$monat.$jahr - $time Uhr ::\n\n";
- 
+
 //Hier werden alle Eingabefelder abgefragt
 foreach($_POST as $name => $value) {
    if (in_array($name, $ignore_fields)) {
@@ -37,27 +37,27 @@ foreach($_POST as $name => $value) {
    }
    $msg .= "::: $name :::\n$value\n\n";
 }
- 
- 
- 
+
+
+
 //E-Mail Adresse des Besuchers als Absender
 if ($sendermail_antwort and isset($_POST[$name_von_emailfeld]) and filter_var($_POST[$name_von_emailfeld], FILTER_VALIDATE_EMAIL)) {
    $email_from = $_POST[$name_von_emailfeld];
 }
- 
+
 $header="From: $email_from";
- 
+
 if (!empty($mail_cc)) {
    $header .= "\n";
    $header .= "Cc: $mail_cc";
 }
- 
+
 //Email als UTF-8 senden
 $header .= "\nContent-type: text/plain; charset=utf-8";
- 
+
 $mail_senden = mail($empfaenger,$betreff,$msg,$header);
- 
- 
+
+
 //Weiterleitung, hier konnte jetzt per echo auch Ausgaben stehen
 if($mail_senden){
   header("Location: ".$url_ok); //Mail wurde gesendet
--- src/components/PriceList/index.js
+++ src/components/PriceList/index.js
@@ -1,83 +1,86 @@
 export const PriceList = () => {
-  return (
-    <div className="pt-24 md:w-160 m-0 m-auto px-0">
-      <div className="text-center ">
-        <h1 className="text-3xl mb-20 font-Atma">Preisliste</h1>
-        <h2 className="text-xl mb-4 font-bold">Kindergeburtstage</h2>
-        <p className="mb-8">
-          Hier finden Sie unsere gängigen Angebote für einen Kindergeburtag.
-          Individuelle Anpassungen auf Ihre Wünsche sind in der Regel kein
-          Problem.
-        </p>
-      </div>
+    return (
+        <div className='pt-24 md:w-160 m-0 m-auto px-0'>
+            <div className='text-center '>
+                <h1 className='text-3xl mb-20 font-Atma'>Preisliste</h1>
+                <h2 className='text-xl mb-4 font-bold'>Kindergeburtstage</h2>
+                <p className='mb-8'>
+                    Hier finden Sie unsere gängigen Angebote für einen
+                    Kindergeburtag. Individuelle Anpassungen auf Ihre Wünsche
+                    sind in der Regel kein Problem.
+                </p>
+            </div>
 
-      <div className="m-0 m-auto max-w-screen-lg flex justify-center pb-8">
-        <div className="pr-16">
-          <p>10 Kinder für 3 Stunden</p>
-          <p>10 Kinder für 4 Stunden</p>
-          <p>Jedes weitere Kind</p>
-          <p>Jede weitere 1/2 Stunde</p>
-          <p>
-            Selbstgemachte Pizza auf dem <br />
-            Blech für alle, Belag nach Wunsch
-          </p>
-        </div>
-        <div className="">
-          <p>155€</p>
-          <p>175€</p>
-          <p>15€</p>
-          <p>15€</p>
-          <p>60€</p>
-        </div>
-      </div>
-      <div className="text-center font-bold pb-4">
-        <p className="mb-16">
-          Im Preis enthalten sind: Eine Hofführung, ausgiebiges Programm mit den
-          Tieren (Eselreiten, füttern, streicheln etc.), viele interessante
-          Infos und Lernen zum Thema Hof, Tier und Natur. Heustalltoben, Nutzung
-          des Geburtstagsraums mit Küche, Bad, und großem Raum fürs gemeinsame
-          Essen und Geschenke auspacken. Wasser und Apfelschorle stellen wir,
-          außerdem Kaffee für die Erwachsenen. Je nach Saison und Zeit außerdem:
-          Bauerngarten ernten, Schnitzeljagd, Bachwanderung. In jedem Fall: Ein
-          unvergesslicher Tag mit vielen tollen Erinnerungn und Natur pur;)
-        </p>
-        <h2 className="text-xl mb-4 font-bold">Hofeintritt</h2>
-        <p className="mb-4">
-          Wenn Sie nicht im Rahmen eines Kindergeburtstages oder anderen
-          gebuchten Veranstaltung bei uns sind, sondern z.B. mit ihrer Familie
-          vorbeikommen möchten, nehmen wir einen Hofeintritt. Der Hofbesuch ist
-          nur mit einer Voranmeldung möglich.
-        </p>
-      </div>
+            <div className='m-0 m-auto max-w-screen-lg flex justify-center pb-8'>
+                <div className='pr-16'>
+                    <p>10 Kinder für 3 Stunden</p>
+                    <p>10 Kinder für 4 Stunden</p>
+                    <p>Jedes weitere Kind</p>
+                    <p>Jede weitere 1/2 Stunde</p>
+                    <p>
+                        Selbstgemachte Pizza auf dem <br />
+                        Blech für alle, Belag nach Wunsch
+                    </p>
+                </div>
+                <div className=''>
+                    <p>170€</p>
+                    <p>190€</p>
+                    <p>15€</p>
+                    <p>15€</p>
+                    <p>60€</p>
+                </div>
+            </div>
+            <div className='text-center font-bold pb-4'>
+                <p className='mb-16'>
+                    Im Preis enthalten sind: Eine Hofführung, ausgiebiges
+                    Programm mit den Tieren (Eselreiten, füttern, streicheln
+                    etc.), viele interessante Infos und Lernen zum Thema Hof,
+                    Tier und Natur. Heustalltoben, Nutzung des Geburtstagsraums
+                    mit Küche, Bad, und großem Raum fürs gemeinsame Essen und
+                    Geschenke auspacken. Wasser und Apfelschorle stellen wir,
+                    außerdem Kaffee für die Erwachsenen. Je nach Saison und Zeit
+                    außerdem: Bauerngarten ernten, Schnitzeljagd, Bachwanderung.
+                    In jedem Fall: Ein unvergesslicher Tag mit vielen tollen
+                    Erinnerungn und Natur pur;)
+                </p>
+                <h2 className='text-xl mb-4 font-bold'>Hofeintritt</h2>
+                <p className='mb-4'>
+                    Wenn Sie nicht im Rahmen eines Kindergeburtstages oder
+                    anderen gebuchten Veranstaltung bei uns sind, sondern z.B.
+                    mit ihrer Familie vorbeikommen möchten, nehmen wir einen
+                    Hofeintritt. Der Hofbesuch ist nur mit einer Voranmeldung
+                    möglich.
+                </p>
+            </div>
 
-      <div className="m-0 m-auto max-w-screen-lg flex justify-center pb-16">
-        <div className="pr-16">
-          <p>
-            Hofeintritt mit Führung pro Person
-            <br />
-            (wenn es Ihr erster Besuch ist)
-          </p>
-          <p>
-            Einzeleintritt pro Person
-            <br />
-            (nur möglich wenn Sie schon <br /> einmal bei uns waren)
-          </p>
+            <div className='m-0 m-auto max-w-screen-lg flex justify-center pb-16'>
+                <div className='pr-16'>
+                    <p>
+                        Hofeintritt mit Führung pro Person
+                        <br />
+                        (wenn es Ihr erster Besuch ist)
+                    </p>
+                    <p>
+                        Einzeleintritt pro Person
+                        <br />
+                        (nur möglich wenn Sie schon <br /> einmal bei uns waren)
+                    </p>
+                </div>
+                <div>
+                    <p>20€</p>
+                    <br />
+                    <br />
+                    <p>10€</p>
+                </div>
+            </div>
+            <div className='text-center'>
+                <h2 className='text-xl mb-4 font-bold'>Kreativworkshop</h2>
+                <p>
+                    Preise für einen Kreativworkshop wie Holzflöße basteln, oder
+                    z.B. Fosterhocker oder Teelicht-Häuschen töpfern können
+                    individuell vereinbart werden.
+                </p>
+            </div>
         </div>
-        <div>
-          <p>20€</p>
-          <br />
-          <br />
-          <p>10€</p>
-        </div>
-      </div>
-      <div className="text-center">
-        <h2 className="text-xl mb-4 font-bold">Kreativworkshop</h2>
-        <p>
-          Preise für einen Kreativworkshop wie Holzflöße basteln, oder z.B.
-          Fosterhocker oder Teelicht-Häuschen töpfern können individuell
-          vereinbart werden.
-        </p>
-      </div>
-    </div>
-  );
+    );
 };
